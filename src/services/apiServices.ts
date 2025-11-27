5ff1da94c34036689d85984a68cf22f8d4b24fe9
Tariq Rasheed <xs2tariqrasheed@gmail.com>
2025-11-27 23:35:37 +0500

    tempate fixed

--- src/services/apiServices.ts
+++ src/services/apiServices.ts
@@ -9,7 +9,7 @@ const isProd = import.meta.env.VITE_ENV === "prod";
 
 export const getErrorMessage = (error: string) => {
 
-  return `Error:
+  return `Error: \n
 Sorry. We encountered an error. Kindly report the following error message to EidApp@EliteNY.com.  We apologize for the inconvenience. \n
 
 System response:
